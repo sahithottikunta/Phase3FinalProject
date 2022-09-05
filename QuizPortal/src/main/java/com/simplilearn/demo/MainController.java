589d809d1c2275447a195654a3165a3a50d55380
sahithottikunta <sahithottikunta1234@gmail.com>
2022-09-05 15:52:41 +0530

    Phase3Final

---
+++ QuizPortal/src/main/java/com/simplilearn/demo/MainController.java
@@ -0,0 +1,15 @@
+package com.simplilearn.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/")
+    public String hello(){
+        return "Hello World";
+    }    
+
+}
